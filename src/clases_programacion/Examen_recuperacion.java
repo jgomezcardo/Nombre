c449ecce0115a133f858cee130d8d6f2d44eeda5
Ceinmark <Ceinmark@DESKTOP-J0PN6TT>
2021-02-08 09:16:07 +0100

    MENSAJE CON SENTIDO

---
+++ src/clases_programacion/Examen_recuperacion.java
@@ -0,0 +1,80 @@
+package clases_programacion;
+
+public class Examen_recuperacion {
+	
+	public static void main(String arg[]) {
+		
+//		Crear un algoritmo en el que declarada la cadena de texto “Los murciélagos estaban
+//		anclados en el techo sedientos de sangre”, muestre por pantalla cada carácter y el código ASCII
+//		equivalente de ese carácter. (char caracter)
+
+//		String frase ="Los murcielagos estaban anclados en el techo sedientos de sangre";
+//				
+//				for(int i=0;i<frase.length();i++) {
+//					System.out.println("el caracter "+ (frase.charAt(i)+1-1) +" es la letra "+ frase.charAt(i));
+//				}
+		
+//		Crear un algoritmo utilizando sentencias de repetición que muestre por pantalla la
+//		siguiente secuencia de caracteres:
+		
+//		String res ="";
+//		
+//		for (int i =1; i<=5; i ++){
+//			res= "";
+//			for (int j=1; j<=5; j++) {
+//				
+//				if(i==j) {
+//					res+=" * ";
+//				}else {
+//					res+=" + " ;
+//				}
+//			}	
+//			System.out.println(res);	
+//		}
+
+		
+		
+		
+		
+//		Crear un algoritmo que nos permita controlar la entrada de un parking de coches. El
+//		parking cuenta con 3 plantas y en cada planta entran 50 coches, por lo que tiene un total de
+//		150 plazas. Cuando un vehículo quiera entrar se tomara el peso del vehículo, si este es mayor
+//		de 500kg, se indicara que debe aparcar en la tercera planta, por el contrario, si es menor o
+//		igual podrá aparcar en las plantas 1 y 2.		
+				
+//		int peso=(int)(Math.random()*500)+150;
+//		int plazas=(int)((Math.random()*200)+2);
+//		int plazas1=(int)((Math.random()*100));
+//		int plazas2=(int)((Math.random()*100));
+//		int plazas3=(int)((Math.random()*100));
+//		
+//		if(plazas<150) {
+//			if(peso<500) {
+//				if(plazas1<50) {
+//					System.out.println("tu coche pesa: " +peso + " Puede aparcar en la primera planta,"+plazas1+" plazas en uso");
+//				}if(plazas1>50) {
+//					System.out.println("planta 1 llena, aparque en la planta 2");
+//				}else{
+//				if(plazas2<50) {
+//					System.out.println("tu coche pesa: "+peso+ "Puede aparcar en la segunda planta,"+plazas2+" plazas en uso\"");
+//				}else {
+//					System.out.println("planta 2 llena, aparque en la planta 1");
+//				}
+//				}
+//			}else {
+//				if(peso>500) {
+//			if(plazas3<50) {
+//				
+//				System.out.println("tu coche excede el peso estandar ("+ peso+") usted puede aparcar en planta 3, "+plazas3+" plazas en uso" );
+//			}else {
+//				System.out.println("tu coche excede el peso estandar ("+ peso+") planta 3 llena:"+plazas3+",no puede entrar");
+//			}
+//				}else {System.out.println("Parking lleno hay: "+plazas+ " vehiculos, no puede pasar");
+//		
+//				}
+//			}
+//		}	
+		
+		
+	}		
+}	
